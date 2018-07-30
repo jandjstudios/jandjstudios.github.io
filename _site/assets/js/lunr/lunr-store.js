88f9e9807280fe6f7ff2f8336437d27d6c49848e
jandjstudios <jandjstudiosllc@gmail.com>
2018-07-29 18:02:44 -0700

    Commit build of site instead of the serve 'build'

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "",
         "excerpt":"datum-Weather datum a piece of information a fixed starting point of a scale or operationweather the state of the atmosphere at a place and time as regards heat, dryness, sunshine, wind, rain, etc.features Atmel SAMD21G18 microcontroller Bosch Sensortec BME280 environmental sensor Onboard EEPROM settings storage URI style command interface JSON...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/datum/datum-Weather/",
+        "url": "jandjstudios.io/datum/datum-Weather/",
         "teaser":null},{
         "title": "datum",
         "excerpt":"a piece of information a fixed starting point of a scale or operationThe goal of this project is to make it easier for students and hobbyists to incorporate sensors into their projects by doing the heavy lifting of translating the language the sensors speak into a format that’s easy for...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/datum/",
+        "url": "jandjstudios.io/datum/",
         "teaser":null},{
         "title": "Coming soon, datum-Weather!",
         "excerpt":"The first product in the datum series will be coming to GroupGets soon! ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/datum-Weather/",
+        "url": "jandjstudios.io/datum-Weather/",
         "teaser":null}]
